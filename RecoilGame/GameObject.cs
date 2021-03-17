b3289ae1ef71a562886626bbfed6ce598305bbed
Trevor Dunn <thetrevordunn@gmail.com>
2021-03-17 16:23:31 -0400

    GameObject and PlayerWeapon Set Up

--- RecoilGame/GameObject.cs
+++ RecoilGame/GameObject.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Trevor Dunn       3/17/21
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +10,44 @@ namespace RecoilGame
 {
     class GameObject
     {
+        //Fields
+        private Texture2D sprite;
+        private Rectangle rectangle;
+        private bool isActive;
+
+        //CONSTRUCTOR
+
+        /// <summary>
+        /// GameObject's Constructor
+        /// </summary>
+        /// <param name="sprite">Texture For Object</param>
+        /// <param name="rectangle">Rectangle For Object</param>
+        /// <param name="isActive">Set Whether Object Is Active</param>
+        public GameObject(Texture2D sprite, Rectangle rectangle, bool isActive)
+        {
+            this.sprite = sprite;
+            this.rectangle = rectangle;
+            this.isActive = isActive;
+        }
+
+        //PROPERTIES
+
+        /// <summary>
+        /// Rectangle Property
+        /// </summary>
+        public Rectangle Rectangle
+        {
+            get { return rectangle; }
+            set { rectangle = value; }
+        }
+
+        /// <summary>
+        /// IsActive Property
+        /// </summary>
+        public bool IsActive
+        {
+            get { return isActive; }
+            set { isActive = value; }
+        }
     }
 }
--- RecoilGame/PlayerWeapon.cs
+++ RecoilGame/PlayerWeapon.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿//Trevor Dunn       3/17/21
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RecoilGame
 {
-    class PlayerWeapon
+    abstract class PlayerWeapon : GameObject
     {
+        //Fields
+        private float cooldown;
+        private int numOfProjectiles;
+
+        //CONSTRUCTOR
+
+        /// <summary>
+        /// Constructor for PlayerWeapon
+        /// </summary>
+        /// <param name="sprite">Texture For Weapon's Sprite</param>
+        /// <param name="rectangle">Rectangle For Weapon's Position</param>
+        /// <param name="isActive">Bool For If Weapon Is Active</param>
+        /// <param name="cooldown">Float For Weapon's Cooldown</param>
+        /// <param name="numOfProjectiles">Int For Number of Projectiles</param>
+        public PlayerWeapon (Texture2D sprite, Rectangle rectangle, bool isActive, float cooldown, int numOfProjectiles) : base(sprite, rectangle, isActive)
+        {
+            this.cooldown = cooldown;
+            this.numOfProjectiles = numOfProjectiles;
+        }
+
+        //METHODS
+
+        /// <summary>
+        /// Abstract Method For Shoot, Must Be Overriden By Weapons
+        /// </summary>
+        public abstract void Shoot();
     }
 }

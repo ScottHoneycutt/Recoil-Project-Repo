3dc008094096c139584779b6168ba8dd5034aaaf
Scott Honeycutt <scotth.4249@gmail.com>
2021-09-16 22:39:04 -0400

    committing to undo

--- RecoilGame/LevelManager.cs
+++ RecoilGame/LevelManager.cs
@@ -104,7 +104,7 @@ namespace RecoilGame
             
             //Setting objectiveTile to null until one appears in a level----
             objectiveTile = null;
-            numberOfLevels = 7;
+            numberOfLevels = 10;
 
             //Loading in sprites and spritefonts----
             testSprite = game.Content.Load<Texture2D>("square");
